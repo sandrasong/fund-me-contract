20ed6c707f7387614b9f42ab570e6d8e5193c55d
sandrasong <chromsketch@gmail.com>
2024-05-15 19:13:35 -0400

    Add a hardhat task(accounts).

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, task } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+  for (const account of accounts) {
+    const balance = await hre.ethers.provider.getBalance(account)
+    console.log(`${account.address} | balance: ${balance}`)
+  }
+})
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-};
+  solidity: "0.8.8",
+}
 
-export default config;
+export default config
