6814c28853744765767fc56848b139580a788fbb
sandrasong <chromsketch@gmail.com>
2024-05-16 12:25:53 -0400

    Add helper config file.

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import { network } from "hardhat"
+import { networkConfig } from "../helper-hardhat-config"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre
---
+++ helper-hardhat-config.ts
@@ -0,0 +1,11 @@
+const networkConfig = {
+  11155111: {
+    name: "sepolia",
+    ethUsdPriceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+  },
+  31337: {
+    name: "localhost",
+  },
+}
+
+export { networkConfig }
